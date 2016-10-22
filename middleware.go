1ba52c02681fc2cc007796cc48ad2945c1fab694
Johannes Neuhaus <johannes.neuhaus@entenhausen.io>
2016-10-22 12:36:46 +0200

    fix

--- middleware.go
+++ middleware.go
@@ -1,13 +1,10 @@
 package grout
 
-import (
-	"errors"
-	"net/http"
-)
+import "errors"
 
 // MiddlewareHandler is a function manipulating a request. If an error occures (err != nil) it is sent to the client. Useful for e.g. authentication
 // It is guaranteed that r is never nil. This is the case that the route does not exist and thus an 404 is issued before.
-type MiddlewareHandler func(req *http.Request, res *Response, route *Route) error
+type MiddlewareHandler func(req *Request, res *Response, route *Route) error
 
 // Middleware holds meta data useful for debugging as well as the handler which is ought to be called. All middlewares run before executing the
 type Middleware struct {
@@ -26,7 +23,7 @@ func (r *Router) AddMiddleware(name string, mw MiddlewareHandler) *Middleware {
 
 // -- internal middlewares
 
-func checkRouteValidity(req *http.Request, res *Response, route *Route) error {
+func checkRouteValidity(req *Request, res *Response, route *Route) error {
 	if route == nil {
 		// issue 404
 		res.Status = 404
