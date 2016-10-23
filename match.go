b4f7b1e71b07068b90d57f700903f543e0a8cd19
Johannes Neuhaus <johannes.neuhaus@entenhausen.io>
2016-10-23 16:49:45 +0200

    parameter change incl. query parsing

--- match.go
+++ match.go
@@ -1,6 +1,9 @@
 package grout
 
-import "strings"
+import (
+	"net/url"
+	"strings"
+)
 
 // Matcher describes a strategy for matching
 type Matcher interface {
@@ -10,8 +13,9 @@ type Matcher interface {
 
 // MatchResult is the result from matching an URL against a Matcher
 type MatchResult struct {
-	URLParams   map[string]string
-	RouteParams map[string]string
+	URLParams   map[string][]string
+	RouteParams map[string][]string
+	URL         *url.URL
 }
 
 //
@@ -27,14 +31,23 @@ func getDefaultMatcher() Matcher {
 }
 
 // DefaultMatcher is up to change. It only detects url paramters and takes trailing slashes into account. A parameter is indicated by placing ":" in
-// the route. A route which can be matched is for example /user/4 if there is a route /user/:id. Afterwards there will be a mapentry for id which maps to 4.
+// the route. A route which can be matched is for example /user/4 if there is a route /user/:id. Afterwards there will be a mapentry for id which maps to {4}.
+// Multiple parameters are separated by ",". For example for /user/4,5,6 id maps to {4,5,6}
 func DefaultMatcher(uri string, r *Route) *MatchResult {
 	// extract route components
+	url, err := url.ParseRequestURI(uri)
+	if err != nil {
+		Log.Error("An error occured in net/url package during url query parsing", "Error", err.Error())
+	}
+
 	pattern := r.GetPattern()
 	uriComponents := strings.Split(uri, "/")
+	uriComponents[len(uriComponents)-1] = strings.Split(uriComponents[len(uriComponents)-1], "?")[0] // throw away query
 	patternComponents := strings.Split(pattern, "/")
 
-	result := &MatchResult{RouteParams: make(map[string]string), URLParams: make(map[string]string)}
+	url.Query()
+
+	result := &MatchResult{RouteParams: make(map[string][]string), URLParams: make(map[string][]string)}
 
 	if len(uriComponents) != len(patternComponents) {
 		return nil
@@ -44,7 +57,7 @@ func DefaultMatcher(uri string, r *Route) *MatchResult {
 		patternComp := patternComponents[i]
 		if strings.Index(patternComp, ":") == 0 {
 			// routeParameter to match
-			result.RouteParams[patternComp[1:]] = component
+			result.RouteParams[patternComp[1:]] = strings.Split(component, ",")
 		} else {
 			// no parameter to match, if this route matches, the components must be equal
 			if patternComp != component {
@@ -52,6 +65,7 @@ func DefaultMatcher(uri string, r *Route) *MatchResult {
 			}
 		}
 	}
-
+	result.URLParams = url.Query()
+	result.URL = url
 	return result
 }
